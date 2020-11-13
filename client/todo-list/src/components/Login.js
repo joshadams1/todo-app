89ba67c73cb0ba6bf4b83896f846d067f9529d35
joshadams1 <joshadams10@gmail.com>
2020-11-13 16:58:38 -0700

    Remove tester function

--- client/todo-list/src/components/Login.js
+++ client/todo-list/src/components/Login.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { FormControl, TextField, Button } from '@material-ui/core';
 
 const Login = () => {
-    const directToHome = () => { <Link to="./Home.js"></Link> }
     return (
         <div>
             <h1>Home</h1>
