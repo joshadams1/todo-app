f8b503a1f1231a3a93ac0743b35a6f9eddc93038
joshadams1 <joshadams10@gmail.com>
2020-11-13 13:14:08 -0700

    Create initial components + remove public presets

---
+++ client/todo-list/src/components/Login.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default Login;
